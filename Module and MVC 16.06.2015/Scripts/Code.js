572f595cb451018328b0cba2b9b704b3763f99f9
Himanshu <Himanshu@HARY>
2016-06-17 03:01:04 +0530

    Angular Modul programming in code.js  there are 1st module is created then in module we added two controllers

---
+++ Module and MVC 16.06.2015/Scripts/Code.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="angular.js" />
+var x = angular.module("pocmod", []);
+x.controller("poctrl", function ($scope) {
+    var cources = ["ASP.NEt MVC", "Java", "PHP", "Angular"];
+    $scope.CData = cources;
+});
+x.controller("democtrl", function ($scope) {
+    var projects = ["ServicesProject", "RealatorProject", "Other"];
+    $scope.PData = projects;
+});
